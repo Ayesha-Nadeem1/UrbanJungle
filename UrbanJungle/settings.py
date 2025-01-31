857dd013c654ead6b03284da7bd6db35b134f77b
Mirsilent <75432338+Mirsilent@users.noreply.github.com>
2025-01-31 15:46:45 +0500

    Update settings.py

--- UrbanJungle/settings.py
+++ UrbanJungle/settings.py
@@ -106,11 +106,11 @@ from config import DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',  # or your database engine
-        'NAME': DB_NAME,
-        'USER': DB_USER,
-        'PASSWORD': DB_PASSWORD,
-        'HOST': DB_HOST,
-        'PORT': DB_PORT,
+        'NAME': 'Urban_Jungle',
+        'USER': 'admin',
+        'PASSWORD': 'ATmega32u',
+        'HOST': 'localhost',
+        'PORT': '',
     }
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
