218c470347d459952846e74b3b5f276edc434c67
Ayesha-Nadeem1 <144672650+Ayesha-Nadeem1@users.noreply.github.com>
2025-02-14 11:20:09 +0500

    Update consumers.py

--- users/consumers.py
+++ users/consumers.py
@@ -1,65 +1,70 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from rest_framework_simplejwt.tokens import UntypedToken
+from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
+from django.contrib.auth import get_user_model
+from asgiref.sync import sync_to_async
 
-class PlantStatusConsumer(AsyncWebsocketConsumer):
+User = get_user_model()
+
+class BaseAuthenticatedConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        # Group name based on device ID (used for plant status)
-        device_id = self.scope['url_route']['kwargs']['device_id']
-        self.group_name = f"plant_status_{device_id}"
+        """Authenticate user via JWT and connect to the WebSocket group."""
+        self.user = await self.authenticate_user()
+        if not self.user:
+            await self.close(code=4001)  # Unauthorized access
+            return
 
-        # Add the device to the group
+        self.group_name = self.get_group_name()
         await self.channel_layer.group_add(self.group_name, self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Handle disconnection
-        pass
-
-    async def send_status_update(self, event):
-        # Send plant status update to the frontend
-        await self.send(text_data=json.dumps(event['data']))
-
-class TodoNotificationConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        # Group name for the logged-in user
-        self.group_name = f"user_notifications_{self.scope['user'].id}"
-
-        # Add the user to the group
-        await self.channel_layer.group_add(self.group_name, self.channel_name)
-        await self.accept()
-
-    async def disconnect(self, close_code):
-        # Remove the user from the group
+        """Remove the user from the WebSocket group on disconnect."""
         await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
-    async def send_task_notification(self, event):
-        # Send task notification to the frontend
+    async def authenticate_user(self):
+        """Extract and verify JWT token from the WebSocket headers."""
+        headers = dict(self.scope.get("headers", []))
+        token = headers.get(b"authorization", None)
+
+        if not token:
+            return None
+
+        try:
+            token_str = token.decode("utf-8").split(" ")[1]  # Remove "Bearer" prefix
+            decoded_token = UntypedToken(token_str).payload
+            user_id = decoded_token.get("user_id")
+            return await sync_to_async(User.objects.get)(id=user_id)
+        except (User.DoesNotExist, TokenError, InvalidToken, IndexError):
+            return None
+
+    def get_group_name(self):
+        """To be implemented by subclasses to define group naming logic."""
+        raise NotImplementedError("Subclasses must define `get_group_name` method.")
+
+
+class PlantStatusConsumer(BaseAuthenticatedConsumer):
+    def get_group_name(self):
+        device_id = self.scope['url_route']['kwargs']['device_id']
+        return f"plant_status_{device_id}"
+
+    async def send_status_update(self, event):
         await self.send(text_data=json.dumps(event['data']))
 
-from channels.generic.websocket import AsyncWebsocketConsumer
-import json
 
-class HarvestNotificationConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.user = self.scope["user"]  # Assuming authentication middleware is used
-        self.group_name = f"user_{self.user.id}"
+class TodoNotificationConsumer(BaseAuthenticatedConsumer):
+    def get_group_name(self):
+        return f"user_notifications_{self.user.id}"
 
-        # Add the user to the WebSocket group
-        await self.channel_layer.group_add(
-            self.group_name,
-            self.channel_name,
-        )
+    async def send_task_notification(self, event):
+        await self.send(text_data=json.dumps(event['data']))
 
-        await self.accept()
 
-    async def disconnect(self, close_code):
-        # Remove the user from the WebSocket group
-        await self.channel_layer.group_discard(
-            self.group_name,
-            self.channel_name,
-        )
+class HarvestNotificationConsumer(BaseAuthenticatedConsumer):
+    def get_group_name(self):
+        return f"user_{self.user.id}"
 
     async def notification_message(self, event):
-        # Send the notification to the WebSocket
         message = event["message"]
         await self.send(text_data=json.dumps({"notification": message}))
