7bcc7ffd41bc0da46eb0f4dc6a1e8b37718c14f9
Ayesha Nadeem <ashnadeem123@gmail.com>
2025-03-18 16:39:33 +0500

    update settings.py

--- UrbanJungle/settings.py
+++ UrbanJungle/settings.py
@@ -185,6 +185,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+if not os.path.exists(MEDIA_ROOT):
+    os.makedirs(MEDIA_ROOT, exist_ok=True)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
