15f0d956e251559ac4ebb3ba8527fa92cd883144
Ayesha Nadeem <ashnadeem123@gmail.com>
2025-03-19 19:11:00 +0500

    Update routing.py

--- users/routing.py
+++ users/routing.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .consumers import PlantStatusConsumer,TodoNotificationConsumer,HarvestNotificationConsumer,DeviceNotificationConsumer
 
 websocket_urlpatterns = [
-    path('ws/plant-status/', PlantStatusConsumer.as_asgi()),
+    path('ws/plant-status/<int:device_id>/', PlantStatusConsumer.as_asgi()),
     path('ws/todo-notifications/', TodoNotificationConsumer.as_asgi()),
     path('ws/harvest-notifications/', HarvestNotificationConsumer.as_asgi()),
     path("ws/device-notifications/", DeviceNotificationConsumer.as_asgi()),
